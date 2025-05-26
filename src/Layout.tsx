15ea505c8d21192b251b083c11e0aeb0adeee478
matgdev <math.minp@gmail.com>
2025-05-26 11:56:13 -0300

    update: set layout max width to 1400px

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,17 +1,23 @@
 import "./Layout.css"
 import type { JSX } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 interface LayoutProps {
     title: string,
     children: JSX.Element
 }
 
-export function Layout({title, children}: LayoutProps): JSX.Element {
+export function Layout({ title, children }: LayoutProps): JSX.Element {
     return (
         <Container fluid className="layout-container">
-            <Row className="fs-1 d-flex align-items-center px-5 fw-bold border-bottom border-light border-3 shadow">{title}</Row>
-            <Row className="py-4">{children}</Row>
+            <Row className="fs-1 d-flex align-items-center px-5 fw-bold border-bottom border-light border-3 shadow justify-content-center">
+                <Col className="col-12" style={{ maxWidth: "1400px" }}>
+                    {title}
+                </Col>
+            </Row>
+            <Row className="py-4 align-self-center col-12" style={{ maxWidth: "1400px" }}>
+                {children}
+            </Row>
         </Container>
     );
 }
